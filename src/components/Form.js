1b271af5636a60e39950354e9f6a6d9883047eda
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-11 14:56:45 +0100

    feat: add location field & remove unecessary comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,15 @@
 export const Form = () => {
   return (
     <div className="form">
+      <input type="text" name="location" id="location" value="53.374378, -1.711401"/>
+      <br />
       <textarea id="description" type="text" cols="40" rows="5"
         placeholder="Enter a description of your hike" className="text-field">
       </textarea>
-      <label htmlFor="photo">Upload a photo</label>
+      <br />
+      <label htmlFor="photo">Upload a photo </label>
       <input id="photo" type="file" accept="image/png, image/jpeg" />
+      <br />
       <button type="submit" className="btn">Submit</button>
     </div>
   )
