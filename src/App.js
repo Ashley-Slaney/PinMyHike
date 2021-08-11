1b183dfafdb358c1621bbc2f164c543f280544de
Ashley Slaney <ashleyslaney96@gmail.com>
2021-08-11 11:41:44 +0100

    form setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-// import React from "react";
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
       <header></header>
       <body></body>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+export const Form = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Form
