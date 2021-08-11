ccb5f258a4137727cfe235d17933d0f2a4e9ba20
mikey-roberts <mikey_roberts@outlook.com>
2021-08-11 14:52:02 +0100

    test: save my MyMap snapshot

--- src/components/MyMap.test.js
+++ src/components/MyMap.test.js
@@ -6,18 +6,14 @@ const WrappedMap = withScriptjs(withGoogleMap(MyMap));
 it('renders correctly', () => {
   const tree = renderer
     .create(<WrappedMap 
-        qgoogleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
         process.env.REACT_APP_MAPS_API_KEY
-      }`} />)
+      }`} 
+      loadingElement={<div style={{ height: "100%" }} />}
+      containerElement={<div style={{ height: "100%" }} />}
+      mapElement={<div style={{ height: "100%" }} />}
+      />
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
-
-{/* <WrappedMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_MAPS_API_KEY
-        }`} 
-        loadingElement={<div style={{ height: "100%" }} />}
-        containerElement={<div style={{ height: "100%" }} />}
-        mapElement={<div style={{ height: "100%" }} />}
-        /> */}
