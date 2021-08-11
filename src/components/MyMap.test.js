7ea84d1c35524ca9fe8f1dc13de4c1627d0abff6
Cynthia Fu <yinnycfu@gmail.com>
2021-08-11 13:23:35 +0100

    test: set up testing (sswapping driver-navigator)

---
+++ src/components/MyMap.test.js
@@ -0,0 +1,23 @@
+import { withScriptjs, withGoogleMap } from "react-google-maps";
+import renderer from 'react-test-renderer'
+import MyMap from './maps/MyMap'
+const WrappedMap = withScriptjs(withGoogleMap(MyMap));
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<WrappedMap 
+        qgoogleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+        process.env.REACT_APP_MAPS_API_KEY
+      }`} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+{/* <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_MAPS_API_KEY
+        }`} 
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+        /> */}
