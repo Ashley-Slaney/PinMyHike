4e694cdbb7d40129b496120116b2cc234576370e
Cynthia Fu <yinnycfu@gmail.com>
2021-08-11 17:09:44 +0100

    feat: pin size

--- src/components/maps/MyMap.js
+++ src/components/maps/MyMap.js
@@ -11,6 +11,7 @@ function MyMap() {
         position={{ lat: 55.378052, lng: -3.435973 }}
         icon={{
           url: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Map_marker_font_awesome.svg',
+          scaledSize: new window.google.maps.Size(50, 50)
         }}
         />
   </GoogleMap>  
