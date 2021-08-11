2acf183cac3bf6d22ad3803769bb428f7de98c57
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-08-11 14:32:22 +0100

    test: update test to include photo upload

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,8 @@ export const Form = () => {
       <textarea id="description" type="text" cols="40" rows="5"
         placeholder="Enter a description of your hike" className="text-field">
       </textarea>
+      <label htmlFor="photo">Upload a photo</label>
+      <input id="photo" type="file" accept="image/png, image/jpeg" />
       <button type="submit" className="btn">Submit</button>
     </div>
   )
